1c1514d2e425ffcec838386a08af5f7507f60409
N1keshLama <144378637+N1keshLama@users.noreply.github.com>
2024-04-17 18:42:57 +0000

    Area and perimeter of rectangle implemented

--- Geometry.Library/Class1.cs
+++ Geometry.Library/Class1.cs
@@ -26,3 +26,25 @@ public class Square : IShape
     }
 }
 
+public class Rectangle : IShape
+{
+    private double length;
+    private double breadth;
+
+    public Rectangle(double length, double breadth)
+    {
+        this.length = length;
+        this.breadth = breadth;
+    }
+
+    public double CalculateArea()
+    {
+        return length * breadth;
+    }
+
+    public double CalculatePerimeter()
+    {
+        return 2 * (length + breadth);
+    }
+}
+
